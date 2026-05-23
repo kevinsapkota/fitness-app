457cd913a6725f7a4bc0774d60246a0c1e2684e3
Kevin Sapkota <kevinsapkota2011@gmail.com>
2026-05-23 15:36:23 +0100

    add google search concole verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next"
-import "./globals.css"
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "StreetViz",
@@ -12,16 +12,24 @@ export const metadata: Metadata = {
     apple: "/apple-touch-icon.png",
     shortcut: "/favicon.ico",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Search Console Verification */}
+        <meta
+          name="google-site-verification"
+          content="gai7vNpgeF_KrDeWOg9yU8YoB5XMe-aNRluR99Lkelw"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
-  )
+  );
 }
